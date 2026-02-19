23b7f0cff66672790e910fa9a58763a7999f7eed
Sam Magbo <sammagbo@gmail.com>
2026-02-19 01:47:24 -0300

    fix(backend): remove invalid redis import to fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -15,13 +14,11 @@ import { CommunityModule } from './community/community.module';
             ConfigModule.forRoot({ isGlobal: true }),
             ThrottlerModule.forRootAsync({
                   imports: [ConfigModule],
-                  inject: [ConfigService],
                   useFactory: (config: ConfigService) => ({
                         throttlers: [{
                               ttl: 60000,
                               limit: 10,
                         }],
-                        // storage: new ThrottlerStorageRedisService(config.get('REDIS_URL')),
                   }),
             }),
             PrismaModule,
